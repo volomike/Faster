ef46952b2433ac0288652080e8bd48084f98fe38
volomike <volomike@gmail.com>
2011-07-12 20:09:14 -0700

    Updated version number to reflect change to index.php in the root directory of this project.

--- app/_views/Default/vDefault.php
+++ app/_views/Default/vDefault.php
@@ -79,7 +79,7 @@ margin-left:40px;
 	<div id="content">
 
 		<div id="header">
-		Faster -- A Minimalist PHP MVC Framework v.1.032
+		Faster -- A Minimalist PHP MVC Framework v.1.033
 		</div><!-- #header -->
 
 		<p>&nbsp;</p>
