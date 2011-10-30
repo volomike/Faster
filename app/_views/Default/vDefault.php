f95b4f228620d2c0a940d31cf8882e5c870e82ca
volomike <volomike@gmail.com>
2011-10-30 01:15:26 -0300

    Fixed base url path issue.

--- app/_views/Default/vDefault.php
+++ app/_views/Default/vDefault.php
@@ -79,7 +79,7 @@ margin-left:40px;
 	<div id="content">
 
 		<div id="header">
-		Faster -- A Minimalist PHP MVC Framework v.1.036
+		Faster -- A Minimalist PHP MVC Framework v.1.0361
 		</div><!-- #header -->
 
 		<p>&nbsp;</p>
