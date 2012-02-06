ba9b667bfdf49ba479e245ffeb72c32ce0b9dfad
volomike <volomike@gmail.com>
2012-02-06 14:24:43 -0500

    changed version number

--- app/_views/Default/vDefault.php
+++ app/_views/Default/vDefault.php
@@ -79,7 +79,7 @@ margin-left:40px;
 	<div id="content">
 
 		<div id="header">
-		Faster -- A Minimalist PHP MVC Framework v.1.0369
+		Faster -- A Minimalist PHP MVC Framework v.1.0370
 		</div><!-- #header -->
 
 		<p>&nbsp;</p>
